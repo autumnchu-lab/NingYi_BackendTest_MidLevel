ca5c3af0711ef0de26d7df17ad32b3b72a5d48a7
Autumn Chu <autumn99770@gmail.com>
2026-04-20 12:51:29 +0800

    add json data

--- BackendTest/BackendTest/Models/MyOfficeAcpd.cs
+++ BackendTest/BackendTest/Models/MyOfficeAcpd.cs
@@ -5,16 +5,32 @@ namespace BackendTest.Models;
 
 public partial class MyOfficeAcpd
 {
+    /// <summary>
+    /// 員工編號
+    /// </summary>
+    /// <example>A001</example>
     public string AcpdSid { get; set; } = null!;
 
+    /// <summary>
+    /// 中文姓名
+    /// </summary>
+    /// <example>秋天</example>
     public string? AcpdCname { get; set; }
 
+    /// <summary>
+    /// 中文姓名
+    /// </summary>
+    /// <example>Autumn</example>
     public string? AcpdEname { get; set; }
 
     public string? AcpdSname { get; set; }
 
     public string? AcpdEmail { get; set; }
 
+    /// <summary>
+    /// 狀態 (1:啟用, 0:停用)
+    /// </summary>
+    /// <example>1</example>
     public byte? AcpdStatus { get; set; }
 
     public bool? AcpdStop { get; set; }
