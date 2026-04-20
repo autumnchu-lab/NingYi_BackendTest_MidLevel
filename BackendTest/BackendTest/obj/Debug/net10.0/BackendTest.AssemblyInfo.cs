7235323d53ab1ec058c253e8fcc69405b221ea76
Autumn Chu <autumn99770@gmail.com>
2026-04-20 10:50:27 +0800

    new project

--- BackendTest/BackendTest/obj/Debug/net10.0/BackendTest.AssemblyInfo.cs
+++ BackendTest/BackendTest/obj/Debug/net10.0/BackendTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0882cc415bb74a8f80cd5c3a48b6a4dedadac950")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e047c72795b3c32d8a090258163a23a70aa603f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
